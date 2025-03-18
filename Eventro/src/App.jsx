84615c025ce92dd0ee41e98590f8f13dc4544896
Dearie <eburujacintha@gmail.com>
2025-03-18 17:07:30 +0400

    done with app setup

--- Eventro/src/App.jsx
+++ Eventro/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>Eventro</div>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
   )
 }
 
