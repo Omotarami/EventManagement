ec018e72507e3ad69e896d879db64e1caa20b7ba
Dearie <eburujacintha@gmail.com>
2025-03-31 13:39:58 +0400

    worked on signupform

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
           <Router>
-            <Navbar />
+            {/* <Navbar /> */}
             <Routes>
-              <Route path="/" element={<Home />} />
               <Route path="/" element={<Home />} />
             </Routes>
           </Router>
