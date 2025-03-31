d9e2d32c450603d4426329c288c3dc868b12fa17
Dearie <eburujacintha@gmail.com>
2025-03-31 13:53:48 +0400

    adjusted routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignupForm from './pages/SignupForm';
+import AttendeeSignupForm from './pages/AttendeeSignupForm';
+import OrganizerSignupForm from './pages/OrganizerSignupForm';
 
 const App = () => {
   return (
           <Router>
             {/* <Navbar /> */}
             <Routes>
-              <Route path="/signup" element={<SignupForm />} />
+              <Route path="/attendee-signup" element={<AttendeeSignupForm />} />
+              <Route path="/organizer-signup" element={<OrganizerSignupForm />} />
             </Routes>
           </Router>
   )
