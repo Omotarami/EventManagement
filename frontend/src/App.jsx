5fbe0ca3d902b7a050187024fa44843e07bc92c0
Dearie <eburujacintha@gmail.com>
2025-03-31 14:24:22 +0400

    added icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './global.css';
 import AttendeeSignupForm from './pages/AttendeeSignupForm';
 import OrganizerSignupForm from './pages/OrganizerSignupForm';
+import OnboardingPage from './pages/OnboardingPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
             <Routes>
               <Route path="/attendee-signup" element={<AttendeeSignupForm />} />
               <Route path="/organizer-signup" element={<OrganizerSignupForm />} />
+              <Route path="/onboarding" element={<OnboardingPage />} />
             </Routes>
           </Router>
   )
