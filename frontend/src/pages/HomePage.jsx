0fcf5748b4398c1aaf170e721a96f47ac7cb7a42
Omotarami <awofesotitilola@gmail.com>
2025-04-12 01:24:20 +0400

    Started with the home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,38 @@
 const HomePage = () => {
-  
   return (
     <>
-    <div>
-        <h1 className="text-sm text-teal-600 ml-1 mt-4 bg-amber-700 w-2xl p-2 rounded-lg">Welcome to the homepage my name is Tee</h1>
-    </div>
-    <div>
-
-    </div>
+      <div>
+        <div>
+          <img
+            className="h-screen w-full"
+            src="../public/images/bg.png"
+            alt=""
+          />
+          <div className="absolute inset-0 opacity-60  bg-black"></div>
+          <header>
+            <h1 className="absolute inset-0 text-xl font-bold m-15">
+              <span className=" text-teal-600">EVEN</span>
+              <span className="text-orange-300">TRO</span>
+            </h1>
+            <nav>
+              <a href="#" className="absolute inset-0 m-15 pl-140">
+                PLAN EVENTS
+              </a>
+              <a href="#" className="absolute inset-0 m-15 pl-180">
+                ATTEND EVENTS
+              </a>
+              <a href="#" className="absolute inset-0 m-15 pl-225">
+                LOG IN
+              </a>
+              <a href="#" className="absolute inset-0 m-15 pl-255">
+                SIGN UP
+              </a>
+            </nav>
+          </header>
+          <section className="absolute inset-0 m-70">Discover Events Around You</section>
+        </div>
+      </div>
+      <div></div>
     </>
   );
 };
