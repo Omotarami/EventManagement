0a7ee72549b0c0b9200eb285d84a06a2f6e792d5
Omotarami <awofesotitilola@gmail.com>
2025-04-13 12:48:19 +0400

    repositioned the searchbars

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import {MapPin} from "lucide-react";
 const HomePage = () => {
   return (
     <>
@@ -8,49 +9,76 @@ const HomePage = () => {
             src="../public/images/bg.png"
             alt=""
           />
-        <div className="absolute inset-0 opacity-60 bg-black "></div>
-        <header className="absolute top-10 left-0 w-full">
-        <div className="flex justify-between p-6 max-w-5xl mx-auto">
-          <h1 className="text-xl font-bold">
-            <span className=" text-teal-600">EVEN</span>
-            <span className="text-orange-300">TRO</span>
-          </h1>
-          <nav className=" flex space-x-8 ml-auto text-sm ">
-            <a href="#" className="">
-              PLAN EVENTS
-            </a>
-            <a href="#" className="">
-              ATTEND EVENTS
-            </a>
-            <a href="#" className="">
-              LOG IN
-            </a>
-            <a href="#" className="">
-              SIGN UP
-            </a>
-          </nav>
-          </div>
-        </header>
-        <section>
-          <div className=" absolute inset-0 max-w-5xl mx-auto p-6 py-60 font-light">
-            <h2 className="text-4xl font-medium">
-              Discover <span className="font-bold text-teal-600">Events</span>
-              Around You
-            </h2>
-            <p>Book tickets for concerts,tech events, fashion and more</p>
-          </div>
-          <div className="absolute inset-0  m-40 pt-40 font-light ml-190">
-            <h2 className="text-4xl font-medium">
-              Plan Your Next
-              <span className="font-bold text-orange-300">Event</span>
-            </h2>
-            <p>
-              Plan events with vendors, create tickets and sell the experience
-            </p>
-          </div>
-        </section>
+          <div className="absolute inset-0 opacity-60 bg-black "></div>
+          <header className="absolute top-10 left-0 w-full">
+            <div className="flex justify-between px-5 max-w-5xl mx-auto">
+              <h1 className="text-xl font-bold">
+                <span className=" text-teal-600">EVEN</span>
+                <span className="text-orange-300">TRO</span>
+              </h1>
+              <nav className=" flex space-x-8 ml-auto text-sm">
+                <a href="#" className="">
+                  PLAN EVENTS
+                </a>
+                <a href="#" className="">
+                  ATTEND EVENTS
+                </a>
+                <a href="#" className="">
+                  LOG IN
+                </a>
+                <a href="#" className="">
+                  SIGN UP
+                </a>
+              </nav>
+            </div>
+          </header>
+          <section className="absolute inset-0 flex justify-between max-w-5xl mx-auto px-5">
+            <div className="py-50 ">
+              <h2 className="text-4xl font-normal">
+                Discover{" "}
+                <span className="font-bold text-teal-600">Events </span>
+                Around You
+              </h2>
+              <p className="font-light">
+                Book tickets for concerts,tech events, fashion and more
+              </p>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Search for events"
+                  className="bg-white rounded-lg p-1.5 w-65 text-black bg"
+                ></input>
+                <input
+                  type="text"
+                  placeholder="Choose a location"
+                  className=" bg-white rounded-lg p-1.5 w-40 text-black mx-0.5"
+                ></input>
+              </div>
+            </div>
+            <div className="font-light ml-auto pt-70 text-right">
+              <h2 className="text-4xl font-normal">
+                Plan Your Next
+                <span className="font-bold text-orange-300"> Event</span>
+              </h2>
+              <p>
+                Plan events with vendors, create tickets and sell the experience
+              </p>
+              <div className="flex justify-end gap-1 text-start font-semibold">
+                <p className="bg-white rounded-lg p-1.5 w-35 text-orange-300">
+                  Get started
+                </p>
+                <p className=" bg-orange-300 rounded-lg p-1.5 w-25 text-white">
+                  Lean more
+                </p>
+              </div>
+            </div>
+          </section>
         </div>
-        </div>
+      </div>
+      <div className="">
+
+      </div>
+
       <div></div>
     </>
   );
