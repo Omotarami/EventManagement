5990e72840567f95b76c2444bd4f02deb5346717
Omotarami <awofesotitilola@gmail.com>
2025-04-20 13:51:17 +0400

    making changes

--- frontend/src/components/Home/Cards.jsx
+++ frontend/src/components/Home/Cards.jsx
@@ -3,6 +3,18 @@
 const Cards = () => {
     return (
     <>
+    <section className="py-16 px-4 max-w-7xl mx-auto">
+    <div className="flex space-x-6 mb-8 text-black">
+      <p>All</p>
+      <p>Today</p>
+      <p>Events</p>
+      <p>Weekends</p>
+      <p>Online</p>
+      <p>Free</p>
+    </div>
+    </section>
+    
+    
      
     </>
   );
