0629faaac6989800d8b46610f2b16f4ba3b863e8
Michael Oduah <oduahmichaelebuka@gmail.com>
2025-04-20 09:37:51 +0100

    feat: implement event management features including create, update, delete, and retrieve events, agendas, schedules, categories, and tickets; add corresponding routes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const AuthController = require("../controller/auth");
-const useCatchErrors = require("../error/catchErrors");
-class AuthRoute {
-  router = express.Router();
-  authController = new AuthController();
+  const express = require("express");
+  const AuthController = require("../controller/auth");
+  const useCatchErrors = require("../error/catchErrors");
+  class AuthRoute {
+    router = express.Router();
+    authController = new AuthController();
 
-  path = "/auth";
+    path = "/auth";
 
-  constructor() {
-    this.initializeRoutes();
+    constructor() {
+      this.initializeRoutes();
+    }
+
+    initializeRoutes() {
+      // test endpoint
+      this.router.post(
+        `${this.path}/user/signup`,
+        useCatchErrors(this.authController.userRegister)
+      );
+      this.router.post(
+        `${this.path}/organizer/signup`,
+        useCatchErrors(this.authController.organizerRegister)
+      );
+      this.router.post(
+        `${this.path}/login`,
+        useCatchErrors(this.authController.login)
+      );
+    }
   }
 
-  initializeRoutes() {
-    // test endpoint
-    this.router.post(
-      `${this.path}/user/signup`,
-      useCatchErrors(this.authController.userRegister)
-    );
-    this.router.post(
-      `${this.path}/organizer/signup`,
-      useCatchErrors(this.authController.organizerRegister)
-    );
-    this.router.post(
-      `${this.path}/login`,
-      useCatchErrors(this.authController.login)
-    );
-  }
-}
-
-module.exports = AuthRoute;
+  module.exports = AuthRoute;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 const App = require("./app");
 const UserRoute = require("./routes/user");
 const AuthRoute = require("./routes/auth");
+const EventRoute = require("./routes/event");
+const CategoryRoute = require("./routes/category");
+const ScheduleRoute = require("./routes/event-schedule");
+const TicketRoute = require("./routes/ticket");
+
 
 const server = new App();
 server.initializedRoutes([
-  // new UserRoute(),
+  new UserRoute(),
   new AuthRoute(),
+  new EventRoute(),
+  new CategoryRoute(),
+  new ScheduleRoute(),
+  new TicketRoute(),
 ]);
 server.listen();
