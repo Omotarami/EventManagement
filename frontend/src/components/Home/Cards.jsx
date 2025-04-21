143051abc99b364bf6dc6a421bc8fd72f769aadc
Omotarami <awofesotitilola@gmail.com>
2025-04-21 15:59:02 +0400

    added the rest of the text under call to action

--- frontend/src/components/Home/Cards.jsx
+++ frontend/src/components/Home/Cards.jsx
@@ -12,7 +12,7 @@ const Cards = () => {
           <p>Online</p>
           <p>Free</p>
         </div>
-        <div className="flex space-x-4 justify-center w-full ">
+        <div className="flex space-x-10 justify-center w-full ">
           <div className="bg-teal-100 w-130 h-50 rounded-lg  pt-12 pl-5">
             <div className="flex items-baseline space-x-3 space-y-3">
               <img src="../public/icons/buy.svg" alt="buy" className="w-7" />
@@ -20,32 +20,41 @@ const Cards = () => {
                 {" "}
                 Buy Tickets and Explore
               </h3>
-              </div>
-              
+            </div>
+            <div>
               <p className="text-xs text-black">
                 Discover exciting events near you and secure your tickets in
                 just a few clicks. Browse by category, location, or interest,
                 and get instant access to the best experiences. With Eventro,
                 finding and attending events has never been easier!
               </p>
+            </div>
           </div>
           <div className="bg-orange-100 w-130 h-50 rounded-lg  pt-12 pl-5">
-          <div className="flex items-baseline space-x-3 space-y-3 ">
+            <div className="flex items-baseline space-x-3 space-y-3">
               <img src="../public/icons/host.svg" alt="buy" className="w-7" />
               <h3 className=" font-medium text-3xl text-black">
                 {" "}
                 Buy Tickets and Explore
               </h3>
-              </div>
-              
+            </div>
+            <div>
               <p className="text-xs text-black">
-                Discover exciting events near you and secure your tickets in
-                just a few clicks. Browse by category, location, or interest,
-                and get instant access to the best experiences. With Eventro,
-                finding and attending events has never been easier!
+                Easily create and manage events while selling tickets
+                seamlessly. Customize ticket types, set pricing, and promote
+                your event to a wider audience. Eventro simplifies the process,
+                making event hosting effortless.
               </p>
+            </div>
           </div>
         </div>
+        <div className=" flex flex-col justify-items-center items-center mt-10 space-y-3">
+          <h1 className=" text-3xl text-black font-medium">
+            Ready to experience Eventro?
+          </h1>
+          <p className="text-black text-center max-w-xl">Discover, connect, and engage with events. Business conferences, tech meetups  to exclusive concerts, we connect you to the best.</p>
+        </div>
+        <footer></footer>
       </section>
     </>
   );
