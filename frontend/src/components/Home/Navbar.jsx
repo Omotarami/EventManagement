0f0c812231989683877e8fe89e17b55e40f70d18
Omotarami <awofesotitilola@gmail.com>
2025-04-28 21:33:03 +0400

    navbar changes

--- frontend/src/components/Home/Navbar.jsx
+++ frontend/src/components/Home/Navbar.jsx
@@ -33,6 +33,10 @@ const Navbar = () => {
             LOGIN
             <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-300 group-hover:w-full transition-all duration-300"></span>
           </Link>
+          <Link to="/login" className="group relative text-white hover:text-orange-300 transition-colors duration-300 text-sm tracking-wide">
+            EXPLORE CATEGORIES
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-300 group-hover:w-full transition-all duration-300"></span>
+          </Link>
           
          
         </nav>
